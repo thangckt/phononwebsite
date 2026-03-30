df2a72416613915be92a42c0e37241cf0debe30c
henriquemiranda <henriquemiranda@users.noreply.github.com>
2026-03-30 12:29:51 +0000

    deploy: 026fcae2b025f544a9ea193c260b5a40cbb7b982

--- main.js
+++ main.js
@@ -69042,6 +69042,7 @@ class VibCrystal {
         this.needsRender = true;
         this.arrows = false;
         this.cell = false;
+        this.shading = true;
         this.paused = false;
         this.initialized = false;
 
@@ -69051,6 +69052,7 @@ class VibCrystal {
 
         this.stats = null;
         this.camera = null;
+        this.pointLight = null;
         this.controls = null;
         this.scene = null;
         this.renderer = null;
@@ -69110,6 +69112,8 @@ class VibCrystal {
         this.bondscolor = 0xffffff;
         this.defaultArrowColor = this.arrowcolor;
         this.defaultBondsColor = this.bondscolor;
+        this.bondColorByAtom = false;
+        this.defaultBondColorByAtom = this.bondColorByAtom;
         this.atomRadiusScale = 1.0;
         this.defaultAtomRadiusScale = this.atomRadiusScale;
         this.defaultBondRadius = this.bondRadius;
@@ -69124,6 +69128,7 @@ class VibCrystal {
         this.atomInstanceRefs = [];
         this.bondobjects = [];
         this.bondmesh = null;
+        this.bondmeshes = [];
         this.bonds = [];
         this.instanceDummy = new Object3D();
         this.captureK = null;
@@ -69299,6 +69304,15 @@ class VibCrystal {
             self.display = dom_combo[0].options[dom_combo[0].selectedIndex].value;
             self.updatelocal();
         };
+    }
+
+    setShadingCheckbox(dom_checkbox) {
+        let self = this;
+        this.shading = dom_checkbox.prop('checked');
+        dom_checkbox.click( function() {
+            self.shading = this.checked;
+            self.updatelocal();
+        } );
     }
 
     setWebmButton(dom_button) {
@@ -69494,6 +69508,7 @@ class VibCrystal {
         domAtomColorInput,
         domArrowColorInput,
         domBondColorInput,
+        domBondColorByAtomCheckbox,
         domAtomRadiusInput,
         domBondRadiusInput,
         domArrowRadiusInput,
@@ -69511,6 +69526,7 @@ class VibCrystal {
         this.dom_atom_color_input = domAtomColorInput;
         this.dom_arrow_color_input = domArrowColorInput;
         this.dom_bond_color_input = domBondColorInput;
+        this.dom_bond_color_by_atom_checkbox = domBondColorByAtomCheckbox;
         this.dom_atom_radius_input = domAtomRadiusInput;
         this.dom_bond_radius_input = domBondRadiusInput;
         this.dom_arrow_radius_input = domArrowRadiusInput;
@@ -69518,6 +69534,11 @@ class VibCrystal {
         this.dom_bond_add_atom_a = domBondAddAtomA;
         this.dom_bond_add_atom_b = domBondAddAtomB;
         this.dom_bond_add_cutoff_input = domBondAddCutoffInput;
+        const updateBondColorInputState = function() {
+            if (domBondColorInput && domBondColorInput.length) {
+                domBondColorInput.prop('disabled', self.bondColorByAtom);
+            }
+        };
 
         if (domAtomList && domAtomList.length) {
             domAtomList.on('click', 'button[data-atom-number]', function() {
@@ -69565,9 +69586,13 @@ class VibCrystal {
             if (domArrowColorInput && domArrowColorInput.length) {
                 self.arrowcolor = self.normalizeColorHex(domArrowColorInput.val(), self.arrowcolor);
             }
-            if (domBondColorInput && domBondColorInput.length) {
+            if (!self.bondColorByAtom && domBondColorInput && domBondColorInput.length) {
                 self.bondscolor = self.normalizeColorHex(domBondColorInput.val(), self.bondscolor);
             }
+            if (domBondColorByAtomCheckbox && domBondColorByAtomCheckbox.length) {
+                self.bondColorByAtom = !!domBondColorByAtomCheckbox.prop('checked');
+            }
+            updateBondColorInputState();
             if (domBondRadiusInput && domBondRadiusInput.length) {
                 self.bondRadius = Math.max(0.01, parseFloat(domBondRadiusInput.val()) || self.bondRadius);
                 domBondRadiusInput.val(self.bondRadius);
@@ -69593,6 +69618,13 @@ class VibCrystal {
                 applyAppearanceSettings();
             });
         }
+        if (domBondColorByAtomCheckbox && domBondColorByAtomCheckbox.length) {
+            domBondColorByAtomCheckbox.prop('checked', this.bondColorByAtom);
+            domBondColorByAtomCheckbox.on('change', function() {
+                applyAppearanceSettings();
+            });
+        }
+        updateBondColorInputState();
 
         if (domAtomColorInput && domAtomColorInput.length) {
             domAtomColorInput.on('change', function() {
@@ -69644,10 +69676,15 @@ class VibCrystal {
         if (domResetBondsButton && domResetBondsButton.length) {
             domResetBondsButton.click(function() {
                 self.bondscolor = self.defaultBondsColor;
+                self.bondColorByAtom = self.defaultBondColorByAtom;
                 self.bondRadius = self.defaultBondRadius;
                 if (self.dom_bond_color_input && self.dom_bond_color_input.length) {
                     self.dom_bond_color_input.val(self.colorToInputHex(self.bondscolor));
                 }
+                if (self.dom_bond_color_by_atom_checkbox && self.dom_bond_color_by_atom_checkbox.length) {
+                    self.dom_bond_color_by_atom_checkbox.prop('checked', self.bondColorByAtom);
+                }
+                updateBondColorInputState();
                 if (self.dom_bond_radius_input && self.dom_bond_radius_input.length) {
                     self.dom_bond_radius_input.val(self.bondRadius);
                 }
@@ -69771,14 +69808,14 @@ class VibCrystal {
 
         //add lights to the camera
         if (this.display == 'vesta') {
-            let pointLight = new PointLight(  0xffffff, 1.2 );
-            pointLight.position.set(1, 1, 1);
-            this.camera.add(pointLight);
+            this.pointLight = new PointLight( 0xffffff, 1.2 );
+            this.pointLight.position.set(1, 1, 1);
         } else {
-            let pointLight = new PointLight( 0xdddddd );
-            pointLight.position.set(1,1,2);
-            this.camera.add(pointLight);
+            this.pointLight = new PointLight( 0xdddddd );
+            this.pointLight.position.set(1, 1, 2);
         }
+        this.pointLight.visible = true;
+        this.camera.add(this.pointLight);
 
         //controls
         this.controls = new TrackballControls( this.camera, this.container0 );
@@ -69932,10 +69969,14 @@ class VibCrystal {
         for (let i=0; i < atom_numbers.length; i++) {
             let n = atom_numbers[i];
             let atomColor = this.getAtomColor(n);
-            if (this.display == 'vesta') {
-                 let material = new MeshPhongMaterial( {reflectivity:1, shininess: 80} );
-                 material.color.copy(atomColor);
-                 this.materials.push( material );
+            if (!this.shading) {
+                let material = new MeshBasicMaterial( { blending: NormalBlending } );
+                material.color.copy(atomColor);
+                this.materials.push( material );
+            } else if (this.display == 'vesta') {
+                let material = new MeshPhongMaterial( {reflectivity:1, shininess: 80} );
+                material.color.copy(atomColor);
+                this.materials.push( material );
             } else {
                 let material = new MeshLambertMaterial( { blending: NormalBlending } );
                 material.color.copy(atomColor);
@@ -70004,6 +70045,7 @@ class VibCrystal {
         this.atomInstanceRefs = [];
         this.bondobjects  = [];
         this.bondmesh = null;
+        this.bondmeshes = [];
         this.arrowobjects = [];
         this.atompos = [];
         this.atomvel = [];
@@ -70099,8 +70141,18 @@ class VibCrystal {
             let axisGeometry  = new CylinderGeometry( this.arrowRadius, this.arrowRadius,
                                                             this.arrowLength );
 
-            let AxisMaterial  = new MeshLambertMaterial( { color: this.arrowcolor,
-                                                                 blending: NormalBlending } );
+            let AxisMaterial;
+            if (this.shading) {
+                AxisMaterial = new MeshLambertMaterial( {
+                    color: this.arrowcolor,
+                    blending: NormalBlending
+                } );
+            } else {
+                AxisMaterial = new MeshBasicMaterial( {
+                    color: this.arrowcolor,
+                    blending: NormalBlending
+                } );
+            }
 
             for (let i=0; i<atoms.length; i++) {
 
@@ -70137,7 +70189,13 @@ class VibCrystal {
             let key = this.getBondRuleKey(a.atom_number, b.atom_number);
             let rule = this.bondRules[key];
             if (rule && length < rule.cutoff) {
-                this.bonds.push({ a: ad, b: bd, baseLength: length });
+                this.bonds.push({
+                    a: ad,
+                    b: bd,
+                    a_atom_number: a.atom_number,
+                    b_atom_number: b.atom_number,
+                    baseLength: length
+                });
                 if (this.display == 'vesta') {
                     let aColor = this.getAtomColor(a.atom_number);
                     let bColor = this.getAtomColor(b.atom_number);
@@ -70151,44 +70209,109 @@ class VibCrystal {
             }
         }
 
-        //build one instanced mesh for all bonds
+        const createBondMaterial = function(vertexColorsEnabled) {
+            let bondMaterialConfig = {
+                color: this.bondscolor,
+                blending: NormalBlending,
+                vertexColors: vertexColorsEnabled
+            };
+            if (this.shading) {
+                return new MeshLambertMaterial(bondMaterialConfig);
+            }
+            return new MeshBasicMaterial(bondMaterialConfig);
+        }.bind(this);
+
+        //build bond meshes
         if (this.bonds.length > 0) {
             let bondGeometry = new CylinderGeometry(
                 this.bondRadius, this.bondRadius, 1.0, this.bondSegments, this.bondVertical, true
             );
-            let bondMaterial = new MeshLambertMaterial({
-                color: this.bondscolor,
-                blending: NormalBlending,
-                vertexColors: this.display == 'vesta'
-            });
 
-            this.bondmesh = new InstancedMesh(bondGeometry, bondMaterial, this.bonds.length);
-            this.bondmesh.name = "bonds";
-            this.bondmesh.instanceMatrix.setUsage(DynamicDrawUsage);
-            this.bondmesh.frustumCulled = false;
+            if (this.bondColorByAtom) {
+                let meshA = new InstancedMesh(bondGeometry, createBondMaterial(true), this.bonds.length);
+                let meshB = new InstancedMesh(bondGeometry, createBondMaterial(true), this.bonds.length);
+                meshA.name = "bonds-a";
+                meshB.name = "bonds-b";
+                meshA.instanceMatrix.setUsage(DynamicDrawUsage);
+                meshB.instanceMatrix.setUsage(DynamicDrawUsage);
+                meshA.frustumCulled = false;
+                meshB.frustumCulled = false;
 
-            for (let i=0; i<this.bonds.length; i++) {
-                let bond = this.bonds[i];
-                let bonddata = getBond(bond.a, bond.b);
-                this.instanceDummy.position.copy(bonddata.midpoint);
-                this.instanceDummy.quaternion.copy(bonddata.quaternion);
-                this.instanceDummy.scale.set(1, bond.baseLength, 1);
-                this.instanceDummy.updateMatrix();
-                this.bondmesh.setMatrixAt(i, this.instanceDummy.matrix);
+                let dir = new Vector3();
+                for (let i=0; i<this.bonds.length; i++) {
+                    let bond = this.bonds[i];
+                    let lengthNow = bond.a.distanceTo(bond.b);
+                    let bonddata = getBond(bond.a, bond.b);
+                    dir.copy(bond.b).sub(bond.a).normalize();
+                    let offset = dir.clone().multiplyScalar(lengthNow * 0.25);
 
-                if (this.display == 'vesta' && this.bondmesh.setColorAt && bondColors[i]) {
-                    this.bondmesh.setColorAt(
-                        i,
-                        new Color(bondColors[i][0], bondColors[i][1], bondColors[i][2])
-                    );
+                    this.instanceDummy.quaternion.copy(bonddata.quaternion);
+                    this.instanceDummy.scale.set(1, lengthNow * 0.5, 1);
+
+                    this.instanceDummy.position.copy(bonddata.midpoint).sub(offset);
+                    this.instanceDummy.updateMatrix();
+                    meshA.setMatrixAt(i, this.instanceDummy.matrix);
+
+                    this.instanceDummy.position.copy(bonddata.midpoint).add(offset);
+                    this.instanceDummy.updateMatrix();
+                    meshB.setMatrixAt(i, this.instanceDummy.matrix);
+
+                    if (meshA.setColorAt) {
+                        meshA.setColorAt(i, this.getAtomColor(bond.a_atom_number));
+                    }
+                    if (meshB.setColorAt) {
+                        meshB.setColorAt(i, this.getAtomColor(bond.b_atom_number));
+                    }
                 }
-            }
 
-            this.bondmesh.instanceMatrix.needsUpdate = true;
-            if (this.display == 'vesta' && this.bondmesh.instanceColor) {
-                this.bondmesh.instanceColor.needsUpdate = true;
+                meshA.instanceMatrix.needsUpdate = true;
+                meshB.instanceMatrix.needsUpdate = true;
+                if (meshA.instanceColor) {
+                    meshA.instanceColor.needsUpdate = true;
+                }
+                if (meshB.instanceColor) {
+                    meshB.instanceColor.needsUpdate = true;
+                }
+
+                this.bondmeshes.push(meshA);
+                this.bondmeshes.push(meshB);
+                this.bondmesh = meshA;
+                this.scene.add(meshA);
+                this.scene.add(meshB);
+            } else {
+                this.bondmesh = new InstancedMesh(
+                    bondGeometry,
+                    createBondMaterial(this.display == 'vesta'),
+                    this.bonds.length
+                );
+                this.bondmesh.name = "bonds";
+                this.bondmesh.instanceMatrix.setUsage(DynamicDrawUsage);
+                this.bondmesh.frustumCulled = false;
+
+                for (let i=0; i<this.bonds.length; i++) {
+                    let bond = this.bonds[i];
+                    let bonddata = getBond(bond.a, bond.b);
+                    this.instanceDummy.position.copy(bonddata.midpoint);
+                    this.instanceDummy.quaternion.copy(bonddata.quaternion);
+                    this.instanceDummy.scale.set(1, bond.baseLength, 1);
+                    this.instanceDummy.updateMatrix();
+                    this.bondmesh.setMatrixAt(i, this.instanceDummy.matrix);
+
+                    if (this.display == 'vesta' && this.bondmesh.setColorAt && bondColors[i]) {
+                        this.bondmesh.setColorAt(
+                            i,
+                            new Color(bondColors[i][0], bondColors[i][1], bondColors[i][2])
+                        );
+                    }
+                }
+
+                this.bondmesh.instanceMatrix.needsUpdate = true;
+                if (this.display == 'vesta' && this.bondmesh.instanceColor) {
+                    this.bondmesh.instanceColor.needsUpdate = true;
+                }
+                this.bondmeshes.push(this.bondmesh);
+                this.scene.add(this.bondmesh);
             }
-            this.scene.add(this.bondmesh);
         }
 
     }
@@ -70205,6 +70328,9 @@ class VibCrystal {
 
     addLights() {
         this.scene.add(this.camera);
+        if (this.pointLight) {
+            this.pointLight.visible = true;
+        }
         let light = new AmbientLight( 0x333333 );
         this.scene.add( light );
     }
@@ -70379,17 +70505,37 @@ class VibCrystal {
             for (let i=0; i<this.bonds.length; i++) {
                 let bond = this.bonds[i];
                 let bonddata = getBond(bond.a, bond.b);
-                this.instanceDummy.position.copy(bonddata.midpoint);
                 this.instanceDummy.quaternion.copy(bonddata.quaternion);
-                this.instanceDummy.scale.set(1, bond.a.distanceTo(bond.b), 1);
-                this.instanceDummy.updateMatrix();
-                this.bondmesh.setMatrixAt(i, this.instanceDummy.matrix);
+                let lengthNow = bond.a.distanceTo(bond.b);
+                if (this.bondColorByAtom && this.bondmeshes.length >= 2) {
+                    let dir = new Vector3().copy(bond.b).sub(bond.a).normalize();
+                    let offset = dir.multiplyScalar(lengthNow * 0.25);
+
+                    this.instanceDummy.scale.set(1, lengthNow * 0.5, 1);
+
+                    this.instanceDummy.position.copy(bonddata.midpoint).sub(offset);
+                    this.instanceDummy.updateMatrix();
+                    this.bondmeshes[0].setMatrixAt(i, this.instanceDummy.matrix);
+
+                    this.instanceDummy.position.copy(bonddata.midpoint).add(offset);
+                    this.instanceDummy.updateMatrix();
+                    this.bondmeshes[1].setMatrixAt(i, this.instanceDummy.matrix);
+                } else if (this.bondmesh) {
+                    this.instanceDummy.position.copy(bonddata.midpoint);
+                    this.instanceDummy.scale.set(1, lengthNow, 1);
+                    this.instanceDummy.updateMatrix();
+                    this.bondmesh.setMatrixAt(i, this.instanceDummy.matrix);
+                }
             }
 
             for (let i=0; i<this.atommeshes.length; i++) {
                 this.atommeshes[i].instanceMatrix.needsUpdate = true;
             }
-            if (this.bondmesh) {
+            if (this.bondmeshes && this.bondmeshes.length) {
+                for (let i=0; i<this.bondmeshes.length; i++) {
+                    this.bondmeshes[i].instanceMatrix.needsUpdate = true;
+                }
+            } else if (this.bondmesh) {
                 this.bondmesh.instanceMatrix.needsUpdate = true;
             }
 
@@ -72164,6 +72310,7 @@ v.setCameraDirectionButton($$1('#cameraz'),'z');
 
 v.setDisplayCombo($$1('#displaystyle'));
 v.setCellCheckbox($$1('#drawcell'));
+v.setShadingCheckbox($$1('#drawshading'));
 v.setWebmButton($$1('#webmbutton'));
 v.setGifButton($$1('#gifbutton'));
 v.setArrowsCheckbox($$1('#drawvectors'));
@@ -72177,6 +72324,7 @@ v.setAdvancedAppearanceControls(
     $$1('#atom_color_input'),
     $$1('#arrow_color_input'),
     $$1('#bond_color_input'),
+    $$1('#bond_color_by_atom_checkbox'),
     $$1('#atom_radius_input'),
     $$1('#bond_radius_input'),
     $$1('#arrow_radius_input'),
