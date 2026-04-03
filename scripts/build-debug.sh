e531a4428c72d5527ed5cb1ca7b7d9a9a92fbf04
Henrique Miranda <miranda.henrique@gmail.com>
2026-04-03 18:13:29 +0200

    Unify homepage and README documentation

--- scripts/build-debug.sh
+++ scripts/build-debug.sh
@@ -4,14 +4,14 @@ set -eu
 ROOT="$(CDPATH= cd -- "$(dirname "$0")/.." && pwd)"
 cd "$ROOT"
 
-python3 python/phononweb/scripts/render_homepage.py
-
 rm -rf build
 mkdir -p build
 
+python3 python/phononweb/scripts/render_homepage.py --output build/index.html
+
 rollup -c
 
-cp -r figures css libs data phonondb2018 README.md index.html phonon.html exciton.html structure.html favicon.svg favicon.ico build/
+cp -r figures css libs data phonondb2018 README.md phonon.html exciton.html structure.html favicon.svg favicon.ico build/
 rm -f build/data/phonondb2017/.gitignore
 cp build/main.js build/main.min.js
 cp build/exciton.js build/exciton.min.js
--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -4,10 +4,10 @@ set -eu
 ROOT="$(CDPATH= cd -- "$(dirname "$0")/.." && pwd)"
 cd "$ROOT"
 
-python3 python/phononweb/scripts/render_homepage.py
-
 rm -rf build
 mkdir -p build/test
+
+python3 python/phononweb/scripts/render_homepage.py --output build/index.html
 
 rollup -c
 
@@ -15,7 +15,7 @@ terser build/main.js -c -m --source-map "content=build/main.js.map,url=main.min.
 terser build/exciton.js -c -m --source-map "content=build/exciton.js.map,url=exciton.min.js.map" -o build/exciton.min.js
 terser build/structure.js -c -m --source-map "content=build/structure.js.map,url=structure.min.js.map" -o build/structure.min.js
 
-cp -r figures css libs data phonondb2018 README.md index.html phonon.html exciton.html structure.html favicon.svg favicon.ico build/
+cp -r figures css libs data phonondb2018 README.md phonon.html exciton.html structure.html favicon.svg favicon.ico build/
 rm -f build/data/phonondb2017/.gitignore
 cp -r test/fixtures build/test/
 
